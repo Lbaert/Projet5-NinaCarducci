912ab6bea2e3f40f28c879b15b68f02e7d9cfd36
PC <lbaert.pro@gmail.com>
2023-07-29 15:50:58 +0200

    modal

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -153,8 +153,8 @@
           index = i ;
         }
       });
-      next =
-        imagesCollection[index] ||
+      preview =
+        imagesCollection[--index] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[++index] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
